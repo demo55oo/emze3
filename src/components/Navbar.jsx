e1d63ab4cb4925e977963cfc2adc0ebadc7f469e
Will Adams <bushblade@gmail.com>
2022-04-18 13:35:33 +0100

    Use Navlink component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,82 @@
-import { useNavigate, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
 
+// NOTE: Here should be using NavLink to detect active path
+// this is only possible since version 6.2.1 and wasn't possilbe in
+// the course code which is why Brad went with the option he did
+
 function Navbar() {
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const pathMatchRoute = (route) => {
-    if (route === location.pathname) {
-      return true
-    }
-  }
-
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon
-              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-            />
-            <p
-              className={
-                pathMatchRoute('/')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }
-            >
-              Explore
-            </p>
+          <li className='navbarListItem'>
+            <NavLink to='/'>
+              {({ isActive }) => (
+                <>
+                  <ExploreIcon
+                    fill={isActive ? '#2c2c2c' : '#8f8f8f'}
+                    width='36px'
+                    height='36px'
+                  />
+                  <p
+                    className={
+                      isActive
+                        ? 'navbarListItemNameActive'
+                        : 'navbarListItemName'
+                    }
+                  >
+                    Explore
+                  </p>
+                </>
+              )}
+            </NavLink>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/offers')}>
-            <OfferIcon
-              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-            />
-            <p
-              className={
-                pathMatchRoute('/offer')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }
-            >
-              Offers
-            </p>
+          <li className='navbarListItem'>
+            <NavLink to='/offers'>
+              {({ isActive }) => (
+                <>
+                  <OfferIcon
+                    fill={isActive ? '#2c2c2c' : '#8f8f8f'}
+                    width='36px'
+                    height='36px'
+                  />
+                  <p
+                    className={
+                      isActive
+                        ? 'navbarListItemNameActive'
+                        : 'navbarListItemName'
+                    }
+                  >
+                    Offers
+                  </p>
+                </>
+              )}
+            </NavLink>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/profile')}>
-            <PersonOutlineIcon
-              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-            />
-            <p
-              className={
-                pathMatchRoute('/profile')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }
-            >
-              Profile
-            </p>
+          <li className='navbarListItem'>
+            <NavLink to='/profile'>
+              {({ isActive }) => (
+                <>
+                  <PersonOutlineIcon
+                    fill={isActive ? '#2c2c2c' : '#8f8f8f'}
+                    width='36px'
+                    height='36px'
+                  />
+                  <p
+                    className={
+                      isActive
+                        ? 'navbarListItemNameActive'
+                        : 'navbarListItemName'
+                    }
+                  >
+                    Profile
+                  </p>
+                </>
+              )}
+            </NavLink>
           </li>
         </ul>
       </nav>
