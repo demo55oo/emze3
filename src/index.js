4cdc295e28068f1cbf9378e8a4eb750da84877ca
Will Adams <bushblade@gmail.com>
2022-04-29 11:57:04 +0100

    remove strict mode - bugs with Swiper

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+// NOTE: running in strict mode causes bugs/warnings with Swiper
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
